cc57e650cfc182a46fc9c567e08984cbb18aedc3
Alexandre <ryxandy@hotmail.com>
2021-04-14 14:54:26 -0300

    A bad solution

--- src/PersonalizedExceptions/Main.java
+++ src/PersonalizedExceptions/Main.java
@@ -32,23 +32,17 @@ public class Main {
 			System.out.println("What's the checkOut date? (dd/MM/yyyy)");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			
-			asdasdsa
-			sadasd
-			
-			if (checkIn.before(now) || checkOut.before(now)){
-				System.out.println("Error em Reservation: Reservation must be after now");
-			}else if (!checkOut.after(checkIn)) {
-				System.out.println("Error em Reservation: Reservation must be after now");
-			}else {
-				reservation.updateDates(checkIn, checkOut);
-				System.out.println("Reservation: " + reservation);
+				String error = reservation.updateDates(checkIn, checkOut);
+				if (error != null) {
+					System.out.println("Error in reservation: " + error);
+				}else {
+					System.out.println("Reservation: " + reservation);
+				}
+				
 			}
-		}
-		
-		
-		
 		sc.close();
+		}	
+		
 	}
-}
+
+
--- src/PersonalizedExceptions/Reservation.java
+++ src/PersonalizedExceptions/Reservation.java
@@ -41,9 +41,16 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);	
 	}
 	
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)){
+			return "Reservation dates for updates must be future";
+		}else if (!checkOut.after(checkIn)) {
+			return "Reservation dates for updates must be future";
+		}else
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 
 	@Override
